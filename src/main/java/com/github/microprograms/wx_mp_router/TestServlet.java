a6c0cba81cb69e49eec64e50408c3cce9bd12956
xuzewei <microprograms@126.com>
2018-03-05 16:51:01 +0800

    v0.0.4

--- src/main/java/com/github/microprograms/wx_mp_router/TestServlet.java
+++ src/main/java/com/github/microprograms/wx_mp_router/TestServlet.java
@@ -7,12 +7,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @WebServlet("/test")
 public class TestServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(TestServlet.class);
     private static final long serialVersionUID = 1L;
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        log.info("test -> Hello World!");
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().println("Hello World!");
