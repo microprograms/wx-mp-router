9795d21fb0978dcad82656ebc6735ffd54f73ae8
xuzewei <microprograms@126.com>
2018-03-05 16:23:53 +0800

    v0.0.2

---
+++ src/main/java/com/github/microprograms/wx_mp_router/TestServlet.java
@@ -0,0 +1,20 @@
+package com.github.microprograms.wx_mp_router;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("Hello World!");
+    }
+}
