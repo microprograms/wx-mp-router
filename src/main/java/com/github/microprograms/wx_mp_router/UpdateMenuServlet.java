20b81bb4c73d7b4bd1111ffd0c5a0a6bc96606be
xuzewei <microprograms@126.com>
2018-05-11 10:52:26 +0800

    v0.0.22

--- src/main/java/com/github/microprograms/wx_mp_router/UpdateMenuServlet.java
+++ src/main/java/com/github/microprograms/wx_mp_router/UpdateMenuServlet.java
@@ -9,14 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.github.microprograms.wx_mp_router.utils.Fn;
 
 import me.chanjar.weixin.common.api.WxConsts.MenuButtonType;
 import me.chanjar.weixin.common.bean.menu.WxMenu;
 import me.chanjar.weixin.common.bean.menu.WxMenuButton;
+import me.chanjar.weixin.common.exception.WxErrorException;
 
 @WebServlet("/updateMenu")
 public class UpdateMenuServlet extends HttpServlet {
@@ -26,21 +32,56 @@ public class UpdateMenuServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
         log.info("updateMenu");
+        JSONObject resp = new JSONObject();
+        resp.put("code", 0);
+        resp.put("message", "成功");
+        String wxMenuButtonsString = request.getParameter("wxMenuButtons");
         try {
-            response.setContentType("text/html;charset=utf-8");
-            response.setStatus(HttpServletResponse.SC_OK);
-            WxMenu wxMenu = new WxMenu();
-            List<WxMenuButton> buttons = new ArrayList<>();
-            WxMenuButton loginButton = new WxMenuButton();
-            loginButton.setName("登录");
-            loginButton.setType(MenuButtonType.VIEW);
-            loginButton.setUrl("http://car-carat-h5.chekela.com/index.html");
-            buttons.add(loginButton);
-            wxMenu.setButtons(buttons);
-            Fn.getWxMpService().getMenuService().menuCreate(wxMenu);
-            response.getWriter().println("更新成功");
+            JSONArray wxMenuButtons = JSON.parseArray(wxMenuButtonsString);
+            updateWxMenu(wxMenuButtons);
         } catch (Exception e) {
             log.error("", e);
+            resp.put("code", 1);
+            resp.put("message", "失败");
+            resp.put("stackTrace", ExceptionUtils.getStackTrace(e));
+        } finally {
+            response.setContentType("application/json;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println(resp.toJSONString());
         }
+    }
+
+    private static void updateWxMenu(JSONArray wxMenuButtons) throws WxErrorException {
+        WxMenu wxMenu = new WxMenu();
+        List<WxMenuButton> buttons = new ArrayList<>();
+        for (int i = 0; i < wxMenuButtons.size(); i++) {
+            JSONObject wxMenuButton = wxMenuButtons.getJSONObject(i);
+            String parentId = wxMenuButton.getString("parentId");
+            if (StringUtils.isBlank(parentId)) {
+                WxMenuButton button = new WxMenuButton();
+                button.setName(wxMenuButton.getString("name"));
+                button.setType(MenuButtonType.VIEW);
+                button.setUrl(wxMenuButton.getString("url"));
+                button.setSubButtons(buildSubButtons(parentId, wxMenuButtons));
+                buttons.add(button);
+            }
+        }
+        wxMenu.setButtons(buttons);
+        Fn.getWxMpService().getMenuService().menuCreate(wxMenu);
+    }
+
+    private static List<WxMenuButton> buildSubButtons(String parentId, JSONArray wxMenuButtons) {
+        List<WxMenuButton> subButtons = new ArrayList<>();
+        for (int i = 0; i < wxMenuButtons.size(); i++) {
+            JSONObject wxMenuButton = wxMenuButtons.getJSONObject(i);
+            if (parentId.equals(wxMenuButton.getString("parentId"))) {
+                WxMenuButton subButton = new WxMenuButton();
+                subButton.setName(wxMenuButton.getString("name"));
+                subButton.setType(MenuButtonType.VIEW);
+                subButton.setUrl(wxMenuButton.getString("url"));
+                subButtons.add(subButton);
+            }
+        }
+        return subButtons;
     }
 }
